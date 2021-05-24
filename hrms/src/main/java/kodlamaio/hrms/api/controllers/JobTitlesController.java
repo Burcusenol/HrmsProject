c8d0c674bf7a2cc1f0ae82e196544cfedc3247d9
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-05-24 20:33:15 +0300

    24.05.2021

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.JobTitle;
 
 @RestController
 @RequestMapping("/api/titles")
 public class JobTitlesController {
 	
-	JobTitleService jobTitleService;
+	private JobTitleService jobTitleService;
 
 	@Autowired
 	public JobTitlesController(JobTitleService jobTitleService) {
@@ -23,8 +27,12 @@ public class JobTitlesController {
 	}
 
 	@GetMapping("/getall")
-	public List<JobTitle> getAll()
-	{
+	public DataResult<List<JobTitle>> getAll(){
 		return this.jobTitleService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody JobTitle jobTitle) {
+		return this.jobTitleService.add(jobTitle);
 	}
 }
