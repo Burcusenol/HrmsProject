7a2902fcbeb280a9235772e750f3288cc02cfc5e
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-06-07 22:34:40 +0300

    07.06.2021

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/ResumeService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import kodlamaio.hrms.core.utilities.results.Result;
-import kodlamaio.hrms.entities.dtos.ResumeDto;
-
-public interface ResumeService {
-
-	 Result insert(ResumeDto resumeDto, int candidateId);
-}
