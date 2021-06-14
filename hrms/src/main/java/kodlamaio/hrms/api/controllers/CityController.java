a11b470ef2946350a160ba2c519bfe5bebcea8da
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-06-14 23:11:58 +0300

    14.06.2021

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/CityController.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.CityService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/cities")
+public class CityController {
+
+	private CityService cityService;
+
+	@Autowired
+	public CityController(CityService cityService) {
+		super();
+		this.cityService = cityService;
+	}
+	
+	@GetMapping("/getall")
+	public ResponseEntity<?> getAll(){
+		return ResponseEntity.ok(this.cityService.getAll());
+	}
+}
--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kodlamaio.hrms.business.abstracts.JobTitleService;
 import kodlamaio.hrms.entities.concretes.JobTitle;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/titles")
 public class JobTitlesController {
