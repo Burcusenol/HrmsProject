9edf0156ed67204046f0737f0d8ed41c6c9f2bbe
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-06-15 21:45:31 +0300

    15.06.2021

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kodlamaio.hrms.business.abstracts.JobAdvertisementService;
 import kodlamaio.hrms.entities.concretes.JobAdvertisement;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/jobAdvertisements")
 public class JobAdvertisementController {
