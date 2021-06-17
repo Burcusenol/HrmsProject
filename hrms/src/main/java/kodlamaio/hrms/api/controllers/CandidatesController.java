1e76d044b30fd12e601be72d2f44eeac220cee45
Burcusenol <59020581+Burcusenol@users.noreply.github.com>
2021-06-17 19:48:09 +0300

    17.06.2021

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/CandidatesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/CandidatesController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import kodlamaio.hrms.business.abstracts.CandidateService;
 import kodlamaio.hrms.entities.concretes.Candidate;
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/candidates")
 public class CandidatesController {
--- hrms/src/main/java/kodlamaio/hrms/api/controllers/EmployersController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/EmployersController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kodlamaio.hrms.business.abstracts.EmployerService;
 import kodlamaio.hrms.entities.concretes.Employer;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/employers")
 public class EmployersController {
